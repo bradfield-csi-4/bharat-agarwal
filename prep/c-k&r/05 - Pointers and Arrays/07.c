906767bb8f55d6924c3777e782cdeb56744c89ec
Bharat Agarwal <bharatagarwal@gmail.com>
2022-06-18 11:20:38 +0530

    Refactoring alloc and afree example

--- prep/c-k&r/05 - Pointers and Arrays/07.c
+++ prep/c-k&r/05 - Pointers and Arrays/07.c
@@ -1,20 +1,16 @@
 #define ALLOCSIZE 10000
 
-// Outside of a function, static restrains the visibility
-// of a variable to the current file (compilation unit).
+// static limits the visibility of a variable
+// to the current file
 
-static char buffer[ALLOCSIZE]; // large character array
-static char *free_pointer = buffer;// points to the next free element
+// large character array
+static char buffer[ALLOCSIZE];
 
+// points to the next free element
+static char *free_pointer = buffer;
 
-// calls to afree must be made in opposite order to
-// calls made on afree
-
-// Storage managed by alloc and afree is in a stack
-// last-in, first-out
-
-// malloc doesn't have such restrictions
-
+// returns a pointer to n consecutive character positions
+// which can be used by caller for storing characters
 char *alloc(int size_requested) {
 	if (size_requested + free_pointer >= buffer + ALLOCSIZE) {
 		return 0;
@@ -22,13 +18,10 @@ char *alloc(int size_requested) {
 
 	free_pointer += size_requested;
 	return free_pointer - size_requested;
-	// returns a pointer to n consecutive
-	// character positions, which can be
-	// used by caller for storing characters
 }
 
+// releases storage so it can be reused later
 void afree(char *pointer) {
-	// releases storeage so it can be reused later
 	if (pointer >= buffer && pointer < buffer + ALLOCSIZE) {
 		free_pointer = pointer;
 	}
